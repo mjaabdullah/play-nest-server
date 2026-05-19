1627adfc2c7111572db2d7ed85b33141eca48f85
mjaabdullah <mdjuelahmedabdullah@gmail.com>
2026-05-19 07:38:31 +0600

    Server connected to MongoDB

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 const express = require("express");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
+
+const uri = process.env.MONGO_URI;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+const run = async () => {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    //   await client.close();
+  }
+};
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Hello play-nest-server!");
